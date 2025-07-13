6832d31105fd5fb9e7f9c88de6dc9b8e65587730
Repdo <pedro.blason@outlook.com>
2025-07-12 21:56:09 -0300

    set enemy

--- Assets/Scripts/StateCheckSound.cs
+++ Assets/Scripts/StateCheckSound.cs
@@ -8,7 +8,7 @@ public class StateCheckSound : EnemyBaseState
     [SerializeField] private float chaseSpeed = 2.0f; // Speed of the chase
     [SerializeField] private float stoppingDistance = 2f; // Distance at which the agent stops
     private NavMeshAgent agent; // Reference to the NavMeshAgent component
-    //private Vector3 destination; // Destination for the agent
+    
 
     public override void OnEnterState(Vector3 doorPos)
     {
@@ -17,7 +17,7 @@ public class StateCheckSound : EnemyBaseState
         agent.speed = chaseSpeed; // Set the speed of the agent
         agent.stoppingDistance = stoppingDistance; // Set the stopping distance for the agent
         agent.SetDestination(doorPos);
-        //OpenCloseDoor.OnDoorInteract += DeterminateDestination; // Subscribe to the door interaction event
+        
     }
 
     public override void UpdateState()
@@ -30,14 +30,7 @@ public class StateCheckSound : EnemyBaseState
     public override void OnExitState()
     {
         base.OnExitState();
-        //OpenCloseDoor.OnDoorInteract -= DeterminateDestination; // Unsubscribe from the door interaction event
+        
     }
 
-    //public void DeterminateDestination(OpenCloseDoor door)
-    //{
-        
-    //    destination = door.transform.position;
-    //    agent.SetDestination(destination);
-    //    Debug.Log("Destination set to door position: " + destination);
-    //}
 }
