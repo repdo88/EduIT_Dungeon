9e7afd36615c514341acda0a20930072318efd38
Repdo <pedro.blason@outlook.com>
2025-07-22 23:20:52 -0300

    chase sound

--- Assets/Scripts/StateChase.cs
+++ Assets/Scripts/StateChase.cs
@@ -9,7 +9,8 @@ public class StateChase : EnemyBaseState
     [SerializeField] private float stoppingDistance = 0.1f; // Distance at which the agent stops
     [SerializeField] private Transform Player; // Reference to the player GameObject
     private NavMeshAgent agent; // Reference to the NavMeshAgent component
-
+    [SerializeField] private AudioSource audioSource; // Reference to the AudioSource component
+    [SerializeField] private AudioClip chaseSound; // Sound to play when chasing the player
 
     public override void OnEnterState()
     {
@@ -17,11 +18,20 @@ public class StateChase : EnemyBaseState
         agent = GetComponent<NavMeshAgent>();
         agent.speed = chaseSpeed; // Set the speed of the agent
         agent.stoppingDistance = stoppingDistance; // Set the stopping distance for the agent
-
+        audioSource.loop = true; // Set the audio source to loop
+        audioSource.clip = chaseSound; // Assign the patrol sound to the audio source
+        audioSource.Play(); // Play the patrol sound
     }
 
     public override void UpdateState()
     {
         agent.SetDestination(Player.position);
+    }
+
+    public override void OnExitState()
+    {
+        base.OnExitState();
+        audioSource.Stop(); // Stop the patrol sound if audio source exists
+        audioSource.loop = false; // Disable looping for the audio source
     }
 }
